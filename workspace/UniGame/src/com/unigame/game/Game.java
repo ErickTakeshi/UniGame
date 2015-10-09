1a100168d105a4a6cf93119325e8a52e777ba68b
Gabriel Papke <gabrielpapke@gmail.com>
2015-10-09 20:28:46 -0300

    Space key

--- workspace/UniGame/src/com/unigame/game/Game.java
+++ workspace/UniGame/src/com/unigame/game/Game.java
@@ -23,7 +23,7 @@ public class Game extends Canvas implements Runnable {
 
     private static final long serialVersionUID = 1L;
 
-    public static final int WIDTH = 200;
+    public static final int WIDTH = 380;
     public static final int HEIGHT = WIDTH / 12 * 9;
     public static final int SCALE = 3;
     public static final String NAME = "UniGame";
