c9473c9e9ab7a7fcd3450635b51eb5ad95bc7984
DiegoSilvaOficial <dhyegospfc@gmail.com>
2015-10-30 20:00:11 -0200

    Projeto_Base
    Cenario + gravidade + colisão + tiro e destruição

--- workspace/UniGame/src/com/unigame/game/Game.java
+++ workspace/UniGame/src/com/unigame/game/Game.java
@@ -23,7 +23,7 @@ public class Game extends Canvas implements Runnable {
 
     private static final long serialVersionUID = 1L;
 
-    public static final int WIDTH = 160;
+    public static final int WIDTH = 260;
     public static final int HEIGHT = WIDTH / 12 * 9;
     public static final int SCALE = 3;
     public static final String NAME = "UniGame";
